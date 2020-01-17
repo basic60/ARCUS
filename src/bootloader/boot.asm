56d898d61544a94b962670bc498e53c30b95e4db
basic60 <basic60@outlook.com>
2020-01-18 02:06:23 +0800

    enter long mode

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -4,7 +4,7 @@ mov si, 0
 mov ax, cs
 mov ds, ax
 mov es, ax           
-mov sp, 7c00h
+mov esp, 7b000h
 jmp load_stage2
 
 disk_rw_struct:
@@ -32,11 +32,100 @@ load_stage2:
     push word 50        ; number of blocks
     push dword 1        ; start LBA number
     call read_disk_by_int13h
-    add esp,10
+    add esp, 10
     jmp enter_long_mode
 
 times 510-($-$$) db 0
 dw 0xaa55
 
+%define PAGE_TABLE 0x7b000
+%define CODE_SEG   0x0008
+%define DATA_SEG    0x0010
+
+gdt64:
+.Null:
+    dq 0                              ; Null Descriptor - should be present.
+.Code:
+    dq 0x00209A0000000000             ; 64-bit code descriptor (exec/read).
+    dq 0x0000920000000000             ; 64-bit data descriptor (read/write).
+.pointer:
+    dw $ - gdt64 - 1                    ; 16-bit Size (Limit) of GDT.
+    dd gdt64
+
 enter_long_mode:
+    call fill_page_table
+    call enable_paging
+    lgdt [gdt64.pointer]
+    jmp CODE_SEG:long_mode_entry
+    jmp $
+
+fill_page_table:
+    mov edi, PAGE_TABLE   ; page talbe start at 0x7b000, occupy 20KB memroy and map the first 4MB
+    push edi
+    mov ecx, 0x1400
+    xor eax, eax
+    cld
+    rep stosd          ; zero out 20KB memory
+    pop edi
+
+    lea eax, [es:edi + 0x1000]     
+    or eax, 3 
+    mov [es:edi], eax
+
+    lea eax, [es:edi + 0x2000]
+	or eax, 3 
+    mov [es:edi + 0x1000], eax
+
+    lea eax, [es:edi + 0x3000]        
+	or eax, 3
+    mov [es:edi + 0x2000], eax
+    
+    lea eax, [es:edi + 0x4000]        
+	or eax, 3
+    mov [es:edi + 0x2008], eax
+
+    push edi               
+    lea edi, [es:edi + 0x3000]
+    mov eax, 3 
+    .loop_page_table:
+        mov [es:edi], eax
+        add eax, 0x1000
+        add edi, 8
+        cmp eax, 0x400000       
+        jb .loop_page_table
+    pop edi
+
+    mov ax, 0x2401
+    int 0x15              ; Enalbe A20 address line. 
+    cli
+    ret
+
+enable_paging:
+    ; enable pae and pge
+    mov eax, 10100000b
+    mov cr4, eax
+
+    mov eax, PAGE_TABLE
+    mov cr3, eax
+
+    ; set the long mode bit in the EFER MSR (model specific register)
+    mov ecx, 0xC0000080
+    rdmsr
+    or eax, 0x00000100 
+    wrmsr
+
+    ; enable paging and protection
+    mov eax, cr0
+    or eax, 0x80000001
+    mov cr0, eax
+    ret
+
+[BITS 64]
+long_mode_entry:
+    mov ax, DATA_SEG
+    mov ds, ax       ;DS存放数据段描述符。
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
     jmp $
